05c4724ac0080aa1bd3ebcb05eaf8cec34d2f17a
LichKiller - BIT <adonis.villalva@grupobusiness.it>
2023-07-24 15:19:08 -0500

    Implementacion con ventana y logica de almacenamiento

---
+++ js/popup.js
@@ -0,0 +1,28 @@
+(function ($){
+    $(document).ready(function(){
+        //Muestra la ventana emergente si el usuario no a dado consentimiento.
+        if(!Drupal.settings.cookies_analytics_consent){
+            //Crea una ventana emergente con el botón aceptar.
+            var popUpContent =  '<div class="cookie_analytics_popup">'+
+                                '<p>Este sitio funciona con cookies'+
+                                'Deseas permitir?</p>'
+                                '<button id="cookie_analytics_btn>Aceptar</button></div>';
+            $('body').append(popUpContent);
+            //Agrega efecto Fade in
+            $('.cookie_analytics_popup').fadeIn();
+            //Captura el click Aceptar
+            $('#cookie_analytics_btn').click(function(){
+                //Al aceptar se oculta la ventana emergente
+                $('.cookie_analytics_popup').fadeOut();
+                //Realiza una llamada Ajax para almacenar el consentimiento.
+                $.ajax({
+                    url: 'cookie_analytics/accept',
+                    sucess: function(){
+                        //Recarga la página
+                        location.reload();
+                    }
+                })
+            });
+        }
+    });
+})(jQuery);
