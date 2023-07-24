c0aba5f30fc3985c548e56c2820aef1f3432a236
LichKiller - BIT <adonis.villalva@grupobusiness.it>
2023-07-24 17:22:05 -0500

    Version de verificacion

--- js/popup.js
+++ js/popup.js
@@ -2,20 +2,15 @@
     $(document).ready(function() {
       // Muestra la ventana emergente si el usuario no ha dado su consentimiento.
       if (!Drupal.settings.cookies_analytics_consent) {
-        // Crea una ventana emergente con el botón aceptar.
-        var popUpContent = '<div class="cookie_analytics_popup">' +
-                           '<p>Este sitio funciona con cookies. ¿Deseas permitir?</p>' +
-                           '<button id="cookie_analytics_btn">Aceptar</button></div>';
-        $('body').append(popUpContent);
         // Agrega efecto Fade in
-        $('.cookie_analytics_popup').fadeIn();
+        $('#cookie_analytics_popup').fadeIn();
         // Captura el clic Aceptar
-        $('#cookie_analytics_btn').click(function() {
+        $('#cookie_analytics_aceptar_btn').click(function() {
           // Al aceptar, se oculta la ventana emergente
-          $('.cookie_analytics_popup').fadeOut();
+          $('#cookie_analytics_popup').fadeOut();
           // Realiza una llamada Ajax para almacenar el consentimiento.
           $.ajax({
-            url: '/cookie_analytics/accept',
+            url: '/cookies-analytics/aceptar',
             success: function() {
               // Recarga la página web
               location.reload();
