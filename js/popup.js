8eabcc1db20adc4ac0d14c676f78965c18f5ba90
LichKiller <adonis.villalva@outlook.com>
2023-08-24 16:02:48 -0500

    Test google

--- js/popup.js
+++ js/popup.js
@@ -41,10 +41,30 @@
         popup.style.display = "none";
         modal.style.display = "none";
         localStorage.setItem('cookieConsent', 'true');
-        var gaScript = document.createElement('script');
-        gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + googleAnalyticsID;
-        gaScript.async = true;
-        document.head.appendChild(gaScript);
+        // Google Analytics tracking code
+        (function(i, s, o, g, r, a, m) {
+          i['GoogleAnalyticsObject'] = r;
+          (i[r] =
+            i[r] ||
+            function() {
+              (i[r].q = i[r].q || []).push(arguments);
+            }),
+            (i[r].l = 1 * new Date());
+          (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+          a.async = 1;
+          a.src = g;
+          m.parentNode.insertBefore(a, m);
+        })(
+          window,
+          document,
+          'script',
+          'https://www.googletagmanager.com/gtag/js?id=G-YVRL8S5KS5',
+          'ga'
+        );
+
+        ga('create', 'G-YVRL8S5KS5', 'auto');
+        ga('send', 'pageview');
+        // End of Google Analytics tracking code
         localStorage.setItem('googleConsent', 'true');
       });
       
